b9447b30e059b3d68a79603f036af889610c0704
Leo666111 <leoziggyforrest14@gmail.com>
2024-12-18 04:11:43 +0000

    Create Main.bat

---
+++ Main.bat
@@ -0,0 +1,18 @@
+@echo off
+:: Open Chrome and go to the specified URL
+start chrome https://passwords.google.com/
+
+:: Wait for Chrome to open
+timeout /t 5 >nul
+
+:: Send Ctrl+A to select all
+powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('^{a}')"
+
+:: Send Ctrl+C to copy
+powershell -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('^{c}')"
+
+:: Wait for copying to complete
+timeout /t 2 >nul
+
+echo Copied the content!
+pause
